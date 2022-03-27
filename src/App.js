4b70e99153469f143dae57ddba580bf113cc284f
Hexo97 <57594288+Hexo97@users.noreply.github.com>
2022-03-27 11:29:54 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./Components/Card.js";
+
+const courses = [[],[],]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        
+        <Card
+          title={"Abbas is an amazing guy"}
+          category={"Tech"}
+          description={"amazing person"}
+        />
+        
       </header>
     </div>
   );
