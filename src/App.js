9f911d8ba5578b844c1247591156eb3b4da4c8a5
Hexo97 <57594288+Hexo97@users.noreply.github.com>
2022-04-06 01:04:20 +0300

    adding NavBar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./Components/Card.js";
+import TopNav from "./Components/TopNav.js";
 
-const courses = [[],[],]
+const courses = [[], []];
 
 function App() {
   return (
     <div className="App">
+      <TopNav />
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
-        
         <Card
           title={"Abbas is an amazing guy"}
           category={"Tech"}
           description={"amazing person"}
         />
-        
       </header>
     </div>
   );
