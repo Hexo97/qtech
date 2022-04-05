c6b8085ef2a6b68b7658a5861472ec870b5a28eb
Hexo97 <57594288+Hexo97@users.noreply.github.com>
2022-04-06 01:13:19 +0300

    merging template to original

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Card from "./Components/Card.js";
-import TopNav from "./Components/TopNav.js";
+import React, { useRef, useEffect } from 'react';
+import { useLocation, Switch } from 'react-router-dom';
+import AppRoute from './utils/AppRoute';
+import ScrollReveal from './utils/ScrollReveal';
+import ReactGA from 'react-ga';
 
-const courses = [[], []];
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
 
-function App() {
+// Views 
+import Home from './views/Home';
+
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+
+const trackPage = page => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
+
+const App = () => {
+
+  const childRef = useRef();
+  let location = useLocation();
+
+  useEffect(() => {
+    const page = location.pathname;
+    document.body.classList.add('is-loaded')
+    childRef.current.init();
+    trackPage(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
   return (
-    <div className="App">
-      <TopNav />
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-
-        <Card
-          title={"Abbas is an amazing guy"}
-          category={"Tech"}
-          description={"amazing person"}
-        />
-      </header>
-    </div>
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <Switch>
+          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+        </Switch>
+      )} />
   );
 }
 
