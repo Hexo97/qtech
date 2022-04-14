17bc2f0ca7392d1f84908f0198c3c21ccf2c94af
OmerMhjoub <99316771+OmerMhjoub@users.noreply.github.com>
2022-04-14 22:20:37 +0300

    navbar, logo, react router, product details,
    modify nabvar, add temp logo, react router done, product details almost done

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavBar from './Components/NavBar';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import ProductDetails from './Components/ProductDetails';
+// import Products from './Components/Products';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  <BrowserRouter>
+  <NavBar />
+   <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="ProductDetails" element={<ProductDetails />} />
+    </Routes>
+</BrowserRouter>,
   document.getElementById('root'),
 );
 
