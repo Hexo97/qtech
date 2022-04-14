c71f2b6328c4553d8ef895c394ca1cd1b4cd4206
ABMaybe23 <99318421+ABMaybe23@users.noreply.github.com>
2022-04-14 23:42:51 +0300

    Contacts and about

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
 import React from 'react';
-<<<<<<< Updated upstream
 import './App.css';
 // import Card from './Components/Card.js';
-import NavBar from './Components/NavBar';
-<<<<<<< HEAD
-import Products from './Pages/Products';
-
-// import SlideShow from './Components/SlideShow';
-=======
-import SlideShow from './Components/SlideShow';
->>>>>>> master
-
-function App() {
-  return (
-    <div className="bg-blue-400">
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      <NavBar />
-<<<<<<< HEAD
-      {/* <Products /> */}
-      {/* <SlideShow/> */}
-=======
-      <SlideShow />
->>>>>>> master
-
-      {/* <Card
-          title={"Counselling"}
-          category={"Tech"}
-          description={"amazing person"}
-        /> */}
-=======
+// import NavBar from './Components/NavBar';
+// import Products from './Pages/Products';
 import { Outlet, Link } from 'react-router-dom';
 import 'bootswatch/dist/quartz/bootstrap.min.css';
+// import SlideShow from './Components/SlideShow';
+// import SlideShow from './Components/SlideShow';
+
 
 export default function App() {
   return (
@@ -42,13 +19,15 @@ export default function App() {
           paddingBottom: '1rem',
         }}
       >
+        <Link to="/products">Products</Link>
+        |
+        {' '}
         <Link to="/contact">Contact Us</Link>
         |
         {' '}
         <Link to="/about">About Us</Link>
       </nav>
       <Outlet />
->>>>>>> Stashed changes
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-=======
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
->>>>>>> master
   theme: {
     extend: {},
   },
