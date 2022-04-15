aa555ea2e02c5538584b702778bbe9bce184d9fb
Hexo97 <57594288+Hexo97@users.noreply.github.com>
2022-04-15 19:26:12 +0300

    slide show fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 // import Card from './Components/Card.js';
 import SlideShow from "./Components/SlideShow";
-import Product from "./Components/Product";
+// import Product from "./Components/Product";
 
 function App() {
   return (
     <div className="bg-blue-400">
       {/* <img src={logo} className="App-logo" alt="logo" /> */}
       <SlideShow />
-      <Product />
+      {/* <Product /> */}
 
       {/* <Card
           title={"Counselling"}
