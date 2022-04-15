b6d4767d26017451441961c64f1051f8d410da5c
OmerMhjoub <99316771+OmerMhjoub@users.noreply.github.com>
2022-04-15 17:48:02 +0300

    Update Module.jsx

--- src/Components/Module.jsx
+++ src/Components/Module.jsx
@@ -6,6 +6,7 @@ function Module() {
   return (
     <div className="flex justify-between">
       <h3>Introduction</h3>
+      <h4>This should still be here after a rebase</h4>
       <div className="flex space-x-3">
         <div className="flex space-x-1 items-center lg:text-xl">
           <OndemandVideoIcon />
