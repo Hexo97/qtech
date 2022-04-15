829c244e11d34ac440413cd22dd47f3c3b60ee51
ABMaybe23 <99318421+ABMaybe23@users.noreply.github.com>
2022-04-16 01:17:29 +0300

    Update employees.jsx

--- src/Components/employees.jsx
+++ src/Components/employees.jsx
@@ -9,14 +9,14 @@ export const employees = [
     {
         id: 2,
         name: 'John Appleseed',
-        position: 'CEO',
+        position: 'Manager',
         image: 'https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_1280.jpg',
         description: 'Mr. John Smith was appointed Chief Executive Officer of QTech in 2009. Mr. Smith is also an Executive Director of the Group. He is also a member of the Qatar Youth Association Limited. Previously, he was the vice-chairman of the Macau Energy Saving Association.',
     },
     {
         id: 3,
         name: 'Gabriel Alphonso',
-        position: 'CEO',
+        position: 'Tutor',
         image: 'https://cdn.pixabay.com/photo/2018/03/31/21/18/african-american-3279360_1280.jpg',
         description: 'Mr. John Smith was appointed Chief Executive Officer of QTech in 2009. Mr. Smith is also an Executive Director of the Group. He is also a member of the Qatar Youth Association Limited. Previously, he was the vice-chairman of the Macau Energy Saving Association.',
     },
