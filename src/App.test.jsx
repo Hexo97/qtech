22df350b6708b440bac9ed9840f04b062b78ef6a
Hexo97 <57594288+Hexo97@users.noreply.github.com>
2022-04-16 03:02:46 +0300

    add tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import SlideShow from "./Components/SlideShow";
+import NavBar from "./Components/NavBar";
+import { MemoryRouter } from "react-router-dom";
 
 test("Renders the product details link", () => {
-  render(<App />, { wrapper: BrowserRouter });
-  const linkElement = screen.getByText("Click to view details");
+  render(<MemoryRouter>
+    <App />
+  </MemoryRouter>)
+  const linkElement = screen.getByText("Top Rated Courses");
   expect(linkElement).toBeInTheDocument();
 });
+
+describe('Redner Components:', () => {
+
+  test("NavBar", () => {
+    // render(<NavBar />);
+    render(<MemoryRouter>
+      <NavBar />
+    </MemoryRouter>)
+  });
+
+  test("SlideShow", () => {
+    render(<SlideShow />);
+  });
+
+})
