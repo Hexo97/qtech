d696cdea07b19b716f44ab7ff7799acc46e9c0a4
Hexo97 <57594288+Hexo97@users.noreply.github.com>
2022-04-16 04:57:19 +0300

    Add Drawer

---
+++ src/Components/Burger.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+
+export default function Burger() {
+    return (
+        <Menu>
+            <a className="menu-item" href="/">
+                Home
+            </a>
+            <a className="menu-item" href="/products">
+                Products
+            </a>
+            <a className="menu-item" href="/about">
+                About Us
+            </a>
+            <a className="menu-item" href="/contact">
+                Contact Us
+            </a>
+        </Menu>
+    );
+}
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -11,46 +11,19 @@ function Header() {
 
   return (
     <header className="bg-blue-400">
-      {/* Left side (logo) */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
-        <div
-          //   onClick={() => router.push("/")}
-          className="mt-5 flex items-center flex-grow sm:flex-grow-0 px-2"
-        >
-          <img
-            className="object-contain cursor-pointer h-10"
-            src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-          />
-        </div>
-
-        {/* Search bar */}
-
-        <div className="hidden sm:flex items-center h-12 rounded-md bg-yellow-400 flex-grow hover:bg-yellow-500 cursor-pointer">
-          <input
-            type="text"
-            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
-          />
-          {/* <SearchIcon className="h-12 p-4" /> */}
-        </div>
-
-        {/* Right Side */}
-        <div className="text-white flex items-center text-xs md:text-sm space-x-6 mx-4 whitespace-nowrap ">
-          <p>Hello, </p>
-          <p className="font-extrabold">Account & Lists</p>
-
+        <Link to="/">
           <div
-            //   onClick={() => router.push("/orders")}
-            className="link"
+            className="mt-5 flex items-center flex-grow sm:flex-grow-0 px-2"
           >
-            <p>Returns</p>
-            <p className="font-extrabold">& Orders</p>
+            <img
+              className="object-contain cursor-pointer h-10"
+              src="Images/logo.png"
+            />
           </div>
-        </div>
-      </div>
+        </Link> 
 
-      {/* Bottom Nav */}
       <div className="flex bg-amazon_blue-light text-white text-sm p-2 pl-6 space-x-3 justify-center">
-        <p className="flex">{/* <MenuIcon className="h-6 mr-1" /> */}</p>
         <p className="link">
           <Link to="/">Home</Link>
         </p>
@@ -58,15 +31,14 @@ function Header() {
           <Link to="/products">Products</Link>
         </p>
         <p className="link">
-          Services
-        </p>
-        <p className="link">
           <Link to="/about">About Us</Link>
         </p>
         <p className="link">
           <Link to="/contact">Contact Us</Link>
         </p>
+        </div>
       </div>
+            
     </header>
   );
 }
